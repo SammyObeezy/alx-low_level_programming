26c24dc34bdae9c8ea1a40ff5b4663c0d3fe8989
SammyObeezy <barasam07@gmail.com>
2023-10-11 14:01:30 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 /**
- * main - function to run multiples of 3 0r 5 in numbers below 10224
- * Return:0
+ * main - function to run multiples of 3 0r 5 in numbers below 1024
+ *  Return:0
  */
+
 int main(void)
 {
 	int sum = 0;
+
 	int i;
-	for (i = 0 ; i< 1024 ; i++)
+
+	for (i = 0 ; i < 1024 ; i++)
 	{
-		if (i %3 == 0 || i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 			sum = sum + 1;
 	}
-	printf("%d\n", sum);
-	return(0);
+	printf (" % d\n", sum);
+	return (0);
 }
